4c56f19bf7e044ef26474446bce8e8e5759c759e
ziarmandhost <dziarmand@gmail.com>
2022-11-10 02:23:45 +0200

    feat: base project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Test
+
+### yeawdawda wjdlawlkjhdjklawbjkhawkjhka (gay)
---
+++ Test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Antonio {
+
+};
+
+int main() {
+
+	try {
+		Antonio.isGay()
+	}
+	catch (...) {
+		std::cout << "I know bitch, I know...";
+	}
+
+	return 0;
+}
